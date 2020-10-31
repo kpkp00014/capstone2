a38692403281e5e9f0e8cb0d4f36f05bd335219d
kpkp00014 <kpkp00014@gmail.com>
2020-10-31 23:21:45 +0900

    travis 53

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,5 +1,5 @@
 export const REACT_APP_BASIC_SERVER_URL =
-  "http://videoscrollcaptureapp-env.eba-bd3qppnu.ap-northeast-2.elasticbeanstalk.com";
+  process.env.REACT_APP_BASIC_SERVER_URL;
 export const REACT_APP_DEFAULT_FOLDER_URL =
   "https://xcezsimplestorage.s3.ap-northeast-2.amazonaws.com/upload/256x256.png";
 export const REACT_APP_DEFAULT_IMAGE_URL =
