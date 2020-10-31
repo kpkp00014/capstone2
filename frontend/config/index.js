590d5bf5bebe5a889abf359c822853b4179bd280
kpkp00014 <kpkp00014@gmail.com>
2020-10-31 14:20:40 +0900

    travis 20

--- frontend/config/index.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export default {
-  REACT_APP_BASIC_SERVER_URL: process.env.REACT_APP_BASIC_SERVER_URL,
-  REACT_APP_DEFAULT_IMAGE_URL: process.env.REACT_APP_DEFAULT_IMAGE_URL,
-  REACT_APP_DEFAULT_FOLDER_URL: process.env.REACT_APP_DEFAULT_FOLDER_URL,
-};
